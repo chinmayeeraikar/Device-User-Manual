e6078ff0efc2a742e859e6b589452b48e902c4ce
Rewa <rewashete@gmail.com>
2025-03-19 20:34:18 +0530

    Accomplished ntg after many tries1

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 // import React from "react";
 import "./App.css";
 import NavbarRoutes from "./Routes/NavbarRoutes";
-import Scene from "./Scene";
+import ThreeScene from "./Scene";
 import { AppSidebar } from "./components/Sidebar/AppSidebar";
 import { SidebarProvider, SidebarTrigger } from "./components/ui/sidebar";
-//import "./threescene.tsx"
+import Scene from "./Scene";
 
 const App = () => {
   return (
     <div>
       <SidebarProvider className="absolute">
         <AppSidebar />
-        {/* <SheetPopUp /> */}
         <SidebarTrigger className="-ml-1" />
       </SidebarProvider>
       <NavbarRoutes />
-      <Scene />
+      <ThreeScene />
+      {/* <Scene /> */}
     </div>
   );
 };
--- src/components/Pages/Home.tsx
+++ src/components/Pages/Home.tsx
@@ -1,14 +1,19 @@
 // import React from "react";
 // import { Slider } from "@/components/ui/slider"
 import { Slider } from "../Controls/Slider.tsx";
+// import { AppSidebar } from "../Sidebar/AppSidebar";
+// import { SidebarProvider, SidebarTrigger } from "../ui/sidebar";
 
 export const Home = () => {
   return (
     <div>
-      <h1></h1>
+      {/* <SidebarProvider className="absolute">
+        <AppSidebar />
+        <SidebarTrigger className="-ml-1" />
+      </SidebarProvider> */}
       {/* <Slider defaultValue={[3]} max={50} step={1} /> */}
       <div className="w-1/4 absolute inset-x-1/2 -translate-x-1/2 -inset-y-1/7 inset-y-120">
-      <Slider />
+        <Slider />
       </div>
     </div>
   );
